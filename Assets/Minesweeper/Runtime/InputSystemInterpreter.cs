5817240aba50cb05fbaa0f9a5e1286d82ca1c3f7
abrusle <15620365+abrusle@users.noreply.github.com>
2021-05-07 11:11:24 +0200

    WIP : New Input System

---
+++ Assets/Minesweeper/Runtime/InputSystemInterpreter.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Minesweeper.Runtime
+{
+    public abstract class InputSystemInterpreter : MonoBehaviour
+    {
+        [SerializeField] private InputActionAsset inputActionAsset;
+
+        private InputActionMap _inGameActionMap;
+        protected InGameInputActions InGameActions { get; private set; }
+        protected Vector2 FreeCursorPosition { get; private set; }
+
+        protected virtual void OnEnable()
+        {
+            _inGameActionMap = inputActionAsset.FindActionMap("InGame", true);
+            _inGameActionMap.Enable();
+
+            InGameActions = new InGameInputActions(
+                _inGameActionMap.FindAction("Click Primary", true),
+                _inGameActionMap.FindAction("Click Secondary", true),
+                _inGameActionMap.FindAction("Free Cursor Position", true));
+            
+            InGameActions.clickPrimary.performed += OnClickPrimaryPerformed;
+            InGameActions.clickSecondary.performed += OnClickSecondaryPerformed;
+        }
+
+        protected virtual void OnDisable()
+        {
+            InGameActions.clickPrimary.performed -= OnClickPrimaryPerformed;
+            
+            _inGameActionMap.Disable();
+        }
+
+        protected virtual void Update()
+        {
+            FreeCursorPosition = InGameActions.freeCursorPosition.ReadValue<Vector2>();
+        }
+
+        private void OnClickPrimaryPerformed(InputAction.CallbackContext context)
+        {
+            OnClickPrimary(context.ReadValueAsButton());
+        }
+
+        private void OnClickSecondaryPerformed(InputAction.CallbackContext context)
+        {
+            OnClickSecondary(context.ReadValueAsButton());
+        }
+
+        protected abstract void OnClickPrimary(bool pressed);
+        protected abstract void OnClickSecondary(bool pressed);
+
+        protected readonly struct InGameInputActions
+        {
+            public readonly InputAction
+                clickPrimary, clickSecondary, freeCursorPosition;
+
+            public InGameInputActions(InputAction clickPrimary, InputAction clickSecondary, InputAction freeCursorPosition)
+            {
+                this.clickPrimary = clickPrimary;
+                this.clickSecondary = clickSecondary;
+                this.freeCursorPosition = freeCursorPosition;
+            }
+        }
+    }
+}
---
+++ Assets/Minesweeper/Runtime/LevelInputEvents.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Minesweeper.Runtime
+{
+    public class LevelInputEvents : InputSystemInterpreter
+    {
+        [SerializeField] private new Camera camera;
+        [SerializeField] private Grid levelGrid;
+
+        /// <summary>
+        /// Invoked when the mouse hovers over a new cell.
+        /// </summary>
+        [Space]
+        [SerializeField, Tooltip("Invoked when the mouse hovers over a new cell.")]
+        private UnityEvent<Vector2Int?> onCellHovered;
+
+        /// <summary>
+        /// Invoked when the mouse button is pressed down.
+        /// </summary>
+        [SerializeField, Tooltip("Invoked when the mouse button is pressed down.")]
+        private UnityEvent<Vector2Int> onCellSelected;
+        
+        /// <summary>
+        /// Invoked when the mouse button is released.
+        /// </summary>
+        [SerializeField, Tooltip("Invoked when the mouse button is released.")]
+        private UnityEvent<Vector2Int> onCellDeselected;
+
+        /// <summary>
+        /// Invoked when a cell is clicked.
+        /// </summary>
+        [SerializeField, Tooltip("Invoked when a cell is clicked.")]
+        private UnityEvent<Vector2Int> onCellClicked;
+
+        /// <summary>
+        /// Invoked when the mouse button is pressed down.
+        /// </summary>
+        [SerializeField, Tooltip("Invoked when the mouse button is pressed down.")]
+        private UnityEvent<Vector2Int> onBeforeSpecialClick;
+        
+        /// <summary>
+        /// Invoked when the mouse button is pressed down.
+        /// </summary>
+        [SerializeField, Tooltip("Invoked when the mouse button is pressed down.")]
+        private UnityEvent<Vector2Int> onSpecialClick;
+
+        private Vector2Int _currentCursorGridPos;
+        private Vector2Int? _selectedCell, _specialSelectedCell;
+        
+        // TODO take level bounds into account
+        // TODO handle cursor leaving window
+        // TODO add ESC key to cancel selection
+        // TODO check both clicks pressed at the same time.
+
+        /// <inheritdoc />
+        protected override void Update()
+        {
+            base.Update();
+            var newCursorPos = (Vector2Int) levelGrid.WorldToCell(camera.ScreenToWorldPoint(FreeCursorPosition));
+            if (newCursorPos != _currentCursorGridPos)
+            {
+                _currentCursorGridPos = newCursorPos;
+                onCellHovered.Invoke(_currentCursorGridPos);
+                
+                if (_selectedCell != null && _selectedCell.Value != newCursorPos)
+                {
+                    onCellDeselected.Invoke(_selectedCell.Value);
+                    _selectedCell = null;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void OnClickPrimary(bool pressed)
+        {
+            if (pressed) // button pressed
+            {
+                onCellSelected.Invoke(_currentCursorGridPos);
+                _selectedCell = _currentCursorGridPos;
+            }
+            else if (_selectedCell != null)// button released
+            {
+                if (_selectedCell.Value == _currentCursorGridPos)
+                {
+                    onCellClicked.Invoke(_currentCursorGridPos);
+                }
+
+                onCellDeselected.Invoke(_selectedCell.Value);
+                _selectedCell = null;
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void OnClickSecondary(bool pressed)
+        {
+            // same behavior as primary click but with different events and selectedCell.
+        }
+    }
+}
